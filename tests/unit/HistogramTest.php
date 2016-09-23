abf830c7cd1f413a2ed01dbcc73385cfaa0c56d2
Chris Williams <chris@pureformsolutions.com>
2016-09-23 16:13:53 -0700

    Added support for histogram.

---
+++ tests/unit/HistogramTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of graze/dog-statsd
+ *
+ * Copyright (c) 2016 Nature Delivered Ltd. <https://www.graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://github.com/graze/dog-statsd/blob/master/LICENSE.md
+ * @link    https://github.com/graze/dog-statsd
+ */
+
+namespace Graze\DogStatsD\Test\Unit;
+
+use Graze\DogStatsD\Test\TestCase;
+
+class HistogramTest extends TestCase
+{
+    public function testIncrement()
+    {
+        $this->client->histogram('test_metric',10);
+        $this->assertEquals('test_metric:10|h', $this->client->getLastMessage());
+
+        $this->client->histogram('test_metric',1.2);
+        $this->assertEquals('test_metric:1.2|h', $this->client->getLastMessage());
+    }
+}
