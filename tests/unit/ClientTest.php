0d7cbeee49bd9b26ed9dc80bad8eb2cd699242bb
Harry Bragg <harry.bragg@graze.com>
2017-05-04 14:22:26 +0100

    add a test to ensure failure to close the socket does not get reported

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -15,6 +15,7 @@ namespace Graze\DogStatsD\Test\Unit;
 
 use Graze\DogStatsD\Client;
 use Graze\DogStatsD\Test\TestCase;
+use ReflectionProperty;
 
 class ClientTest extends TestCase
 {
@@ -41,6 +42,26 @@ class ClientTest extends TestCase
         $client = new Client();
         $client->configure([]);
         $client->increment('test', 1);
+        $client = null;
+
+        $this->assertNull($client);
+    }
+
+    public function testDestructionWithInvalidSocket()
+    {
+        // create a connection, kill the udp connection (without changing the socket), attempt to send, should re-connect and send again
+        $client = new Client();
+        $client->configure(['host' => '127.0.0.1']);
+
+        // create the connection
+        $client->increment('metric', 1);
+
+        // close the socket
+        $reflector = new ReflectionProperty(Client::class, 'socket');
+        $reflector->setAccessible(true);
+        $socket = $reflector->getValue($client);
+        fclose($socket);
+
         $client = null;
 
         $this->assertNull($client);
